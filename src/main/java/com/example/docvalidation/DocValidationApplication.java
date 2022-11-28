aaa6700cf955db0df4edaafb294e0385bb0df74e
Adrian <adrianmakowski2121@gmail.com>
2022-11-28 22:15:03 +0100

    add gitignore and pom.xml

---
+++ src/main/java/com/example/docvalidation/DocValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.docvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocValidationApplication.class, args);
+    }
+
+}
