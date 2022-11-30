4546e39365f9b4fcb0f5b1d05ef904fbaa661de8
Adrian <adrianmakowski2121@gmail.com>
2022-11-30 18:07:59 +0100

    added test data

---
+++ src/main/resources/db/changelog/testdata/testdata.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    users(first_Name, last_Name, age)
+VALUES
+    ('Adrian', 'Makowski', 21);
+
+INSERT INTO
+    documents(name_of_cert, company, user_id)
+VALUES
+    ('Google Internetowe rewolucje', 'Google', 1);
+
+
+
