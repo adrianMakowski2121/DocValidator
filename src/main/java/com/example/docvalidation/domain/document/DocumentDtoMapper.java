9f8afe3bd1bda533a39f97d6db97e4e89fbf58e0
Adrian <adrianmakowski2121@gmail.com>
2022-12-10 00:22:17 +0100

    added map method

--- src/main/java/com/example/docvalidation/domain/document/DocumentDtoMapper.java
+++ src/main/java/com/example/docvalidation/domain/document/DocumentDtoMapper.java
@@ -1,10 +1,19 @@
 package com.example.docvalidation.domain.document;
 
 import com.example.docvalidation.domain.document.dto.DocumentDto;
+import com.example.docvalidation.domain.user.User;
+import com.example.docvalidation.domain.user.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DocumentDtoMapper {
+    private final UserRepository userRepository;
+
+    public DocumentDtoMapper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public DocumentDto map(Document document) {
         DocumentDto documentDto = new DocumentDto(
@@ -17,4 +26,13 @@ public class DocumentDtoMapper {
         return documentDto;
     }
 
+    public Document map(DocumentDto dto) {
+        Document document = new Document();
+        document.setId(dto.getId());
+        document.setNameOfCert(dto.getNameOfCert());
+        document.setCompany(dto.getCompany());
+        User user = userRepository.findById(dto.getUserId()).orElseThrow();
+        document.setUser(user);
+        return document;
+    }
 }
